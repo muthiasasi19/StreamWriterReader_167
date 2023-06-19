215f14aa4481bc08f5b9101bb26b8d131e02422e
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-19 01:30:27 -0700

    try

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -17,4 +17,43 @@ int main() {
 	outfile.open(NamaFile + ".txt", ios::out);
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+
+	// Unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		// mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+
+		// loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+
+		// menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+}
+
+	// selesai dalam menulis, sekarang tutup filenya
+	outfile.close();
+
+	// membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+	cout << endl << ">= membuka dan membaca file" << endl;
+
+	//jika file tidak ada maka
+	if (infile.is_open())
+	{
+		// melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan hal berikut
+	else cout << "Unable to open file";
+		return 0;
 }
