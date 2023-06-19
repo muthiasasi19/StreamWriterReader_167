34d5d1d15f3dc7a1877bf17d94fcd86ea5780cdf
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-19 01:45:44 -0700

    exception Standard

---
+++ exeptionStandard/exeptionStandard.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <exception>
+//untuk objek exception yang akan digunakan
+#include <array>
+//untuk objek array yang akan digunakan
+using namespace std;
+
+int main() 
+{
+	cout << "Awal Program" << endl; // penanda 1:...
+	try
+	{
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array integer 3 elemen
+		cout << data.at(4) << endl;
+		//memanggil array elemen ke 5
+	}
+
+	catch (exception& e) {
+	//penangkap menggunakan objek exception
+		cout << e.what() << endl;
+	// akan dieksekusi karena array data hanya memiliki 3 elemen
+	}
+
+	cout << "Baris Program yang terakhir" << endl;
+	// penanda2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+	return 0;
+}
+
