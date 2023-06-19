41258d0188aa94469ef8baa6da35311fa712c00a
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-19 01:10:19 -0700

    membuka mode nulis dan menunjuk file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -10,4 +10,11 @@ int main() {
 	cout << "Masukkan Nama File :";
 	cin >> NamaFile;
 
+	// mmebuka file dalam mode menulis
+	ofstream outfile;
+
+	// menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
